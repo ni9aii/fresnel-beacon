f76a0fb9b8f2564d28223e2e19ef8445871699bc
ni9aii <engineerutron@gmail.com>
2026-05-08 17:57:09 +0500

    phase2-fix: add FreeRTOS.h to config_manager.h and esp_chip_info.h to main.c

--- components/config_manager/include/config_manager.h
+++ components/config_manager/include/config_manager.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdint.h>
+#include "freertos/FreeRTOS.h"
 #include "freertos/semphr.h"
 
 /**
--- main/main.c
+++ main/main.c
@@ -2,6 +2,7 @@
 #include "freertos/task.h"
 #include "esp_log.h"
 #include "esp_system.h"
+#include "esp_chip_info.h"
 #include "esp_mac.h"
 #include "esp_wifi.h"
 #include "led_driver.h"
